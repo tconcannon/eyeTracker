0f7ecaf69136b5c98f3eda75472bd19cd82e0e1d
tconcannon <taycon@seas.upenn.edu>
2016-08-08 15:35:26 -0400

    necessary files

---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+var robot = require("robotjs");
+//Speed up the mouse.
+robot.setMouseDelay(2);
+
+var twoPI = Math.PI * 2.0;
+var screenSize = robot.getScreenSize();
+var height = screenSize.height;
+var width = screenSize.width;
+
+console.log(height,'height')
+console.log(width, 'width')
+io.on('connection', function(socket){
+  socket.on('gaze', function(data){
+    console.log(data, 'data')
+    //Move the mouse across the screen as a sine wave.
+ 
+    // y = height * Math.sin((twoPI * x) / width) + height;
+    if(data && data.y && data.x){
+      robot.moveMouse(data.x, data.y);  
+    }
+    
+  })
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = {
+  app: app,
+  server: server
+};
