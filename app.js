ced14c6a8dbb2ad4d0b03ad5b7ef2520ce318c57
tconcannon <taycon@seas.upenn.edu>
2016-08-09 16:28:18 -0400

    save

--- app.js
+++ app.js
@@ -32,15 +32,25 @@ var robot = require("robotjs");
 var screenSize = robot.getScreenSize();
 var height = screenSize.height;
 var width = screenSize.width;
-
+var x=[];
+var y=[];
 io.on('connection', function(socket){
-  socket.on('gaze', function(data){
+  socket.on('gaze', function(data, clock){
       if(data){
+        // x.push(data.x)
+        // y.push(data.y)
+        // setInterval(function(){
+        //   console.log(x,'x')
+        //   console.log(y,'y')
+        //   // x=[];
+        //   // y=[];
+        // },500)
         robot.moveMouse(data.x, data.y); 
-        //robot.moveMouse(data.x, data.y);  
+        robot.moveMouse(data.x, data.y); 
+        console.log(clock) 
         var lastpos=robot.getMousePos()
-        console.log(data.x, lastpos.x,'x')
-        console.log(data.y, lastpos.y,'y')
+        console.log(data.x, 'eye x', lastpos.x,'mouse x')
+        console.log(data.y, 'eye y', lastpos.y,'mouse y')
       }
         
   })
